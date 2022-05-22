e131bf20a322f1cba7205f20dca70ebafc3471ef
Naveen Mayantha <55675843+naveen-98@users.noreply.github.com>
2022-05-22 15:53:42 +0530

    updated

--- ip.c
+++ ip.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h> /* for strncpy */
+#include <string.h> 
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -19,17 +19,17 @@ main()
 
  fd = socket(AF_INET, SOCK_DGRAM, 0);
 
- /* I want to get an IPv4 IP address */
+ 
  ifr.ifr_addr.sa_family = AF_INET;
 
- /* I want IP address attached to "eth0" */
+ 
  strncpy(ifr.ifr_name, "eth0", IFNAMSIZ-1);
 
  ioctl(fd, SIOCGIFADDR, &ifr);
 
  close(fd);
 
- /* display result */
+
  printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
 
  return 0;
